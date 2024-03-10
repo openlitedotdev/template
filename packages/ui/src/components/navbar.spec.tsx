16235d0d1b3ce109337ec52aa7e8875a78c2b868
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-03-10 10:29:36 -0500

    chore(ui): remove test runner not work

--- packages/ui/src/components/navbar.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import Navbar from './navbar'
-
-it('should render component navbar only example', () => {
-  const DOM = render(<Navbar background="#FFF" />)
-
-  expect(DOM.getByText(/Login/).tagName).toBe('A')
-})
