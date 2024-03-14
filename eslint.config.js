cdb91b19ee2472f210de0c4664cf148b66da5bc6
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-03-13 22:18:05 -0500

    chore: disabled eslint-comments/no-unlimited-disable

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,12 @@ import eslint from '@antfu/eslint-config'
 export default eslint({
   formatters: true,
   jsx: true,
-  ignores: ['api', '*.toml'],
+  ignores: [
+    'api',
+    '*.toml',
+  ],
   rules: {
     'node/prefer-global/process': 'off',
-    'eslint-comments/no-unused-disable': 'off',
+    'eslint-comments/no-unlimited-disable': 'off',
   },
 })
