641e3bdbf86f58c3f8a513f7b58c55f5e53dc0cf
Gerzon05 <gerzonarletrangel@gmail.com>
2024-03-14 17:49:14 -0500

    fix: update login and form styles

--- packages/dashboard/src/routes/login.tsx
+++ packages/dashboard/src/routes/login.tsx
@@ -9,22 +9,22 @@ export default function Login() {
   return (
     <>
       <section
-        className="flex-and-col items-center justify-center md:p-10 w-full"
+        className="flex-and-col items-center justify-center md:p-10 text-sad"
       >
         <div className="flex w-full items-center ">
-          <figure className="hidden lg:block w-1/2">
+          <figure className="hidden md:block w-1/2">
             <img src="/png/hero.png" alt="logo-image" />
           </figure>
           <form
-            className="relative flex-and-col justify-center gap-7 md:justify-evenly w-full lg:w-50% border border-[#E4E7E9] rounded-md shadow-md p-5 md:p-20 lg:min-h-screen"
+            className="relative flex-and-col justify-center style-border gap-7 md:justify-evenly p-5 md:p-20 w-full md:w-50% lg:min-h-screen"
           >
-            <h2 className="text-2xl text-center font-bold text-sad">Commerce</h2>
-            <h3 className="text-2xl text-center text-sad font-bold">Login</h3>
+            <h2 className="text-2xl text-center font-bold">Commerce</h2>
+            <h3 className="text-2xl text-center font-bold">Login</h3>
             <figure>
-              <label className="text-sad">Email</label>
-              <div className="flex-and-col text-sad relative">
+              <label>Email</label>
+              <div className="flex-and-col relative">
                 <input
-                  className="input-border"
+                  className="style-border pl-10 py-1"
                   id="email"
                   type="email"
                   placeholder="jhondoe@gmail.com"
@@ -33,10 +33,10 @@ export default function Login() {
               </div>
             </figure>
             <figure>
-              <label className="text-sad">Password</label>
-              <div className="flex-and-col text-sad relative">
+              <label>Password</label>
+              <div className="flex-and-col relative">
                 <input
-                  className="input-border"
+                  className="style-border pl-10 py-1"
                   id="password"
                   type="password"
                   placeholder="Enter your password"
@@ -44,11 +44,11 @@ export default function Login() {
                 <LockKeyhole className="position-icons" />
               </div>
             </figure>
-            <p className="text-center text-base text-[#3858D6] font-semibold">
+            <p className="text-center text-base text-blue font-semibold">
               Forgot password?
             </p>
             <button
-              className="w-full rounded-md py-2 bg-[#3858D6] text-white font-bold"
+              className="rounded-md py-2 bg-[#3858D6] text-white font-bold"
             >
               Login
             </button>
