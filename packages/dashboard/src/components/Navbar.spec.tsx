8cda5f22c31e6a31353fadc0c01e5a6418f29f70
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-04-02 21:54:09 -0500

    fix(dashboard): remove component unnecessary `navbar.tsx` (#57)

--- packages/dashboard/src/components/Navbar.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-it('should render tag name `button`', () => {
-  render(<button>Hola</button>)
-
-  const button = screen.getByText('Hola')
-
-  expect(button.tagName.toLowerCase()).toBe('button')
-})
