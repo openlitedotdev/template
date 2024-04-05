061f858cbf7c07edca7c7cedac4c6338eab78098
Sebastián García <johancs.mm@gmail.com>
2024-04-04 21:21:18 -0500

    fix(ui): replace class to className

--- packages/ui/src/app/layout.tsx
+++ packages/ui/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" class="dark">
+    <html lang="en" className="dark">
       <body>
         <main>{children}</main>
         <Footer />
