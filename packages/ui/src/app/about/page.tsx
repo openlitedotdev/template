f6ee32f551e092446ab4ea557b02fb6eed249bce
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-04-06 11:17:51 -0500

    feat(ui): add new page about

--- packages/ui/src/app/about/page.tsx
+++ packages/ui/src/app/about/page.tsx
@@ -1,7 +1,20 @@
+const stack = [
+  { title: 'Next.js', description: 'File-based routing, React 18, Fast Refresh, Image Optimization, and more.' },
+  { title: 'TailwindCSS', description: 'TailwindCSS can be extended or replaced with an alternative CSS solution.' },
+  { title: 'Open UI', description: 'Open UI . A modern design system built with Shadcn UI and Tailwind CSS' },
+  { title: 'Tank Stack Ecosystem', description: 'High-quality open-source software for web developers.' },
+]
+
+const modules = [
+  { title: 'Rest API', description: 'Built in Python and its popular FastAPI framework' },
+  { title: 'Dashboard', description: 'Built in react and the Open UI library' },
+  { title: 'Ui', description: 'Built on next with a modern, customizable interface and scalable architecture' },
+]
+
 export default function AboutPage() {
   return (
     <div className="mx-auto max-w-7xl p-8 pb-16">
-      <h1 className="text-3xl font-semibold">About</h1>
+      <h1 className="text-5xl font-bold py-5">About</h1>
       <div className="prose">
         <div>
           <p>
@@ -10,72 +23,28 @@ export default function AboutPage() {
             .
           </p>
         </div>
-        <div><p><b>Codebase</b></p></div>
-        <div>
-          <ul>
-            <li>
-              <b>Next.js 13</b>
-              : File-based routing, React 18, Fast Refresh, Image Optimization, and more.
+        <h2 className="font-bold text-2xl py-3">Stack tech</h2>
+        <ul>
+          {stack.map((tech, i) => (
+            <li key={i} className="py-2">
+              <b className="text-primary font-semibold py-2">
+                {tech.title}
+              </b>
+              <p>{tech.description}</p>
             </li>
-            <li>
-              <b>App Router</b>
-              : Uses React Server Components, Data Cache, and async components.
+          ))}
+        </ul>
+        <h2 className="font-bold text-2xl py-3">Modules</h2>
+        <ul>
+          {modules.map((module, i) => (
+            <li key={i} className="py-2">
+              <b className="text-primary font-semibold py-2">
+                {module.title}
+              </b>
+              <p>{module.description}</p>
             </li>
-            <li>
-              <b>TypeScript</b>
-              : Strongly typed codebase and GraphQL payloads with strict mode.
-            </li>
-            <li>
-              <b>GraphQL best practices</b>
-              : Uses GraphQL Codegen and&nbsp;TypedDocumentString&nbsp;to reduce boilerplate and bundle size.
-            </li>
-            <li>
-              <b>Customizable CSS</b>
-              : TailwindCSS can be extended or replaced with an alternative CSS solution.
-            </li>
-            <li>
-              <b>Tooling included</b>
-              : Comes with ESLint, Prettier, Husky, Lint Staged, and Codegen preconfigured.
-            </li>
-          </ul>
-        </div>
-        <div><p><b>Global:</b></p></div>
-        <div>
-          <ul>
-            <li>Channel switcher and Geo detection (coming soon)</li>
-            <li>Dynamic menu</li>
-            <li>Hamburger menu</li>
-            <li>SEO data</li>
-          </ul>
-        </div>
-        <div><p><b>Checkout:</b></p></div>
-        <div>
-          <ul>
-            <li>Single page checkout (including login)</li>
-            <li>Portable to other frameworks (doesn&apos;t use Next.js components)</li>
-            <li>Adyen integration</li>
-            <li>Stripe integration</li>
-            <li>Customer address book</li>
-            <li>Vouchers and Gift Cards</li>
-          </ul>
-        </div>
-        <div><p><b>Product Catalog:</b></p></div>
-        <div>
-          <ul>
-            <li>Categories</li>
-            <li>Variant selection</li>
-            <li>Product attributes</li>
-            <li>Image optimization</li>
-          </ul>
-        </div>
-        <div><p><b>My Account:</b></p></div>
-        <div>
-          <ul>
-            <li>Order history (coming soon)</li>
-            <li>Order completion</li>
-            <li>Order details</li>
-          </ul>
-        </div>
+          ))}
+        </ul>
       </div>
     </div>
   )
