a47bb5a9a37d95287a1a0ff57e5fa73017f9907f
gerzon05 <gerzonarletrangel@gmail.com>
2024-04-19 11:20:58 -0500

    feat(dashboard): page for create categories

--- packages/dashboard/src/components/pages/categories/create-category.tsx
+++ packages/dashboard/src/components/pages/categories/create-category.tsx
@@ -1,3 +1,4 @@
+import { Button, Input, Label } from '@openui-org/react'
 import { Heading } from '@/components/utils/heading'
 import Layout from '@/layouts/layout'
 
@@ -6,6 +7,24 @@ export function CreateCategory() {
     <Layout>
       <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
         <Heading title="Create Category" description="Create product with style" />
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <Label>Imagenes</Label>
+            {/* <InputFile /> */}
+            <Input type="file" />
+          </div>
+          <div className="flex gap-2">
+            <section className="w-1/2 flex flex-col gap-2">
+              <Label>Nombre</Label>
+              <Input type="text" placeholder="Category name" />
+            </section>
+            <section className="w-1/2 flex flex-col gap-2">
+              <Label>Desciption</Label>
+              <Input type="text" placeholder="Category desciption" />
+            </section>
+          </div>
+          <Button className="w-fit text-base">Create</Button>
+        </form>
       </div>
     </Layout>
   )
