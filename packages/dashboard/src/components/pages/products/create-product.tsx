fb00e64cc412525c72b54d83c74e28671a3158cc
gerzon05 <gerzonarletrangel@gmail.com>
2024-04-19 17:24:24 -0500

    feat(dashboard): added the form for create products

--- packages/dashboard/src/components/pages/products/create-product.tsx
+++ packages/dashboard/src/components/pages/products/create-product.tsx
@@ -1,12 +1,71 @@
+import { Button, Input, Label, ScrollArea, Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@openui-org/react'
 import { Heading } from '@/components/utils/heading'
 import Layout from '@/layouts/layout'
 
 export function CreateProduct() {
   return (
     <Layout>
-      <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
-        <Heading title="Create product" description="Create product with style" />
-      </div>
+      <ScrollArea className="flex-1 space-y-4 h-[85vh]">
+        <div className="px-4 md:px-8 pb-5 overflow-hidden ">
+          <Heading title="Create product" description="Create product with style" />
+          <form className="flex flex-col gap-4 pt-3">
+            <div className="flex flex-col gap-2">
+              <Label>Imagenes</Label>
+              <Input type="file" className="" />
+            </div>
+            <div className="grid sm:grid-cols-2 gap-2">
+              <section className="flex flex-col gap-2">
+                <Label>Nombre</Label>
+                <Input type="text" placeholder="Product name" />
+              </section>
+              <section className="flex flex-col gap-2">
+                <Label>Desciption</Label>
+                <Input type="text" placeholder="Product desciption" />
+              </section>
+              <section className="flex flex-col gap-2">
+                <Label>Price</Label>
+                <Input type="number" placeholder="Price" />
+              </section>
+              <section className="flex flex-col gap-2">
+                <Label>Offer</Label>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select to offer" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="True">True</SelectItem>
+                    <SelectItem value="False">False</SelectItem>
+                  </SelectContent>
+                </Select>
+              </section>
+              <section className="flex flex-col gap-2">
+                <Label>Offer price</Label>
+                <Input type="number" placeholder="Offer price" />
+              </section>
+              <section className="flex flex-col gap-2">
+                <Label>Punctuation</Label>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select to punctuation" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="1">1</SelectItem>
+                    <SelectItem value="2">2</SelectItem>
+                    <SelectItem value="3">3</SelectItem>
+                    <SelectItem value="4">4</SelectItem>
+                    <SelectItem value="5">5</SelectItem>
+                  </SelectContent>
+                </Select>
+              </section>
+              <section className="flex flex-col gap-2">
+                <Label>Brand</Label>
+                <Input type="text" placeholder="Brand" />
+              </section>
+            </div>
+            <Button className="w-fit text-base">Create</Button>
+          </form>
+        </div>
+      </ScrollArea>
     </Layout>
   )
 }
