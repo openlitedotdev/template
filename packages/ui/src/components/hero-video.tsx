e0c0812e4924d3e91990ed951062ec4d5d31d866
castrogarciajs <johancs.mm@gmail.com>
2024-04-20 12:29:58 -0500

    perf(ui): add w-full poster

--- packages/ui/src/components/hero-video.tsx
+++ packages/ui/src/components/hero-video.tsx
@@ -3,12 +3,12 @@ import { Button } from '@openui-org/react/components'
 export default function HeroVideo() {
   return (
     <section className="relative text-white">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-4 z-20">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-4 z-20 text-center">
         <h2 className="text-5xl md:text-6xl lg:text-7xl font-bold">Chill Collection</h2>
         <h4 className="text-2xl">Premium comfort for any season</h4>
         <Button className="w-80 text-lg" variant="secondary">Shop now</Button>
       </div>
-      <video poster="/Homepage-Model-Y-Desktop-Global.avif" muted autoPlay loop playsInline className="opacity-90 aspect-video object-cover">
+      <video poster="/Homepage-Model-Y-Desktop-Global.avif" muted autoPlay loop playsInline className="opacity-90 aspect-video object-cover w-full h-full">
         <source src="/demo-commerce.webm" type="video/webm" />
         <source src="/demo-commerce.mp4" type="video/mp4" />
       </video>
