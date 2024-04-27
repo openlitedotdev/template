c715c5ce1d9781cfeeafa8b0f8b13b337a4a07ca
gerzon05 <gerzonarletrangel@gmail.com>
2024-04-27 12:21:08 -0500

    fix(ui): styles corrected for the mobile version

--- packages/ui/src/components/banners.tsx
+++ packages/ui/src/components/banners.tsx
@@ -7,11 +7,13 @@ import banner from '../../public/banner.webp'
 export default function Banners() {
   return (
     <section className="relative flex items-center justify-end inset-0 w-full">
-      <Image src={banner} alt="Banner image" className="w-full h-full object-center object-cover block" />
-      <div className="absolute right-5 p-6 flex flex-col gap-3 items-center lg:text-white text-center text-black">
-        <h2 className="text-4xl font-bold">GOLF CLOTHING</h2>
-        <p>NOW AVAILABLE ON PUMA.COM</p>
-        <Button variant="secondary">Shop Now</Button>
+      <Image src={banner} alt="Banner image" className="w-full h-52 sm:h-full object-center object-cover block" />
+      <div className="absolute right-0 top-0 bottom-0 left-0 p-6 flex bg-foreground/40 items-center justify-center md:justify-end md:pr-10 text-center text-white">
+        <figure className="flex-col gap-3 items-center justify-center flex">
+          <h2 className="text-xl md:text-4xl font-bold">GOLF CLOTHING</h2>
+          <p>NOW AVAILABLE ON PUMA.COM</p>
+          <Button variant="secondary">Shop Now</Button>
+        </figure>
       </div>
     </section>
   )
--- packages/ui/src/components/hero-video.tsx
+++ packages/ui/src/components/hero-video.tsx
@@ -4,11 +4,11 @@ export default function HeroVideo() {
   return (
     <section className="relative text-white">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-4 z-20 text-center">
-        <h2 className="text-5xl md:text-6xl lg:text-7xl font-bold">Chill Collection</h2>
-        <h4 className="text-2xl">Premium comfort for any season</h4>
-        <Button className="w-80 text-lg" variant="secondary">Shop now</Button>
+        <h2 className="text-3xl md:text-6xl lg:text-7xl font-bold">Chill Collection</h2>
+        <h4 className="text-xl md:text-2xl">Premium comfort for any season</h4>
+        <Button className="w-72 md:w-80 text-lg" variant="secondary">Shop now</Button>
       </div>
-      <video poster="/Homepage-Model-Y-Desktop-Global.avif" muted autoPlay loop playsInline className="opacity-90 aspect-video object-cover w-full h-full">
+      <video poster="/Homepage-Model-Y-Desktop-Global.avif" muted autoPlay loop playsInline className="opacity-90 aspect-video object-cover w-full h-52 sm:h-full">
         <source src="/demo-commerce.webm" type="video/webm" />
         <source src="/demo-commerce.mp4" type="video/mp4" />
       </video>
