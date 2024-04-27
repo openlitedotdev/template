045199c546ae74328badbb517d24ff0c08183156
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-04-26 22:27:32 -0500

    feat(ui): added carousel for products (#184)
    Closes #181

--- packages/ui/src/app/page.tsx
+++ packages/ui/src/app/page.tsx
@@ -1,8 +1,8 @@
-import { ProductList } from '@/components/product-list'
 import Offers from '@/components/offers'
 import ProductsByScores from '@/components/products-by-scores'
 import Banners from '@/components/banners'
 import HeroVideo from '@/components/hero-video'
+import CarouselProduct from '@/components/carousel'
 
 export default function Home() {
   return (
@@ -11,9 +11,8 @@ export default function Home() {
       <Offers />
       <Banners />
       <ProductsByScores />
-      <section className="mx-auto max-w-screen-2xl p-6">
-        <h2 className="sr-only">Product list</h2>
-        <ProductList />
+      <section className="mx-auto p-6">
+        <CarouselProduct />
       </section>
     </>
   )
