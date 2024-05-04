f4af8f064700508fe63800281e2aaba794bf767a
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-05-04 09:44:03 -0500

    chore: remove unnecessary files and update dependencies

--- api/services/banners.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-from api.schemas import models
-
-
-def get(db: Session):
-    banners = db.query(models.Banner).all()
-
-    return banners
--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,8 @@ import eslint from '@antfu/eslint-config'
 
 export default eslint({
   formatters: true,
+  typescript: true,
   jsx: true,
-  ignores: [
-    './api/*',
-    '*.toml',
-  ],
   rules: {
     'node/prefer-global/process': 'off',
     'eslint-comments/no-unlimited-disable': 'off',
