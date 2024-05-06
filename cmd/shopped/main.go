c2396fb317fa33b6c1ff6ba075e9ae658674ea36
Sebastián García <johancs.mm@gmail.com>
2024-05-05 19:54:35 -0500

    feat: add SQLite3 database (#193)

--- cmd/shopped/main.go
+++ cmd/shopped/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/OpenLab-dev/shopped/internal/shopped"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
-	port := 3000
+
 	app := shopped.NewServerShopped()
 
 	app.RegisterShoppedRoutes()
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
 	err := app.Listen(fmt.Sprintf(":%d", port))
 
 	if err != nil {
