54cf9b8e7e7329790a3ac10979063adb5dc63264
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-05-08 18:28:08 -0500

    chore: setup cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
+
+if (process.env.NODE_ENV === 'development')
+  await setupDevPlatform()
 
 export default nextConfig
