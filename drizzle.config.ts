f515e25d90e17153f04b6b1d83334212293e7c9f
castrogarciajs <johancs.mm@gmail.com>
2024-09-02 20:50:06 -0500

    chore: improved drizzle.config.ts to v0.33.0

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,14 @@
-import 'dotenv/config'
-import type { Config } from 'drizzle-kit'
+import { defineConfig } from 'drizzle-kit'
 
-export default {
-  schema: './db/schema.ts',
+import 'dotenv/config'
+
+export default defineConfig({
+  schema: './db/schemas/*',
+  dialect: 'sqlite',
   out: './migrations',
   driver: 'turso',
   dbCredentials: {
-    url: process.env.TURSO_CONNECTION_URL!,
+    url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
-} satisfies Config
+})
